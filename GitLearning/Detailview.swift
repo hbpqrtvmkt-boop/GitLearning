101a34ccb3cfe64d26d66f7ebdbcddcdda05e96e
KEIGO KUROBE <keigo9030@icloud.com>
2026-05-26 13:15:04 +0900

    コンテントヴューへのナビゲーションリンクが追加され、DataVIewを編集

---
+++ GitLearning/Detailview.swift
@@ -0,0 +1,18 @@
+//
+//  Detailview.swift
+//  GitLearning
+//
+//  Created by 黒部恵吾 on 2026/05/26.
+//
+
+import SwiftUI
+
+struct Detailview: View {
+    var body: some View {
+        Text("An App to learn Git")
+    }
+}
+
+#Preview {
+    Detailview()
+}
